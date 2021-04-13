52e64a5109fbdaa61cc48b110306bef27552995f
lucas renan <contato@lucasrenan.com>
2021-04-13 21:10:50 +0200

    POC (#1)
    * Add job1
    
    * Add job2

---
+++ jobs/job1/operator1.py
@@ -0,0 +1,3 @@
+class Operator1(object):
+    def run(self) -> str:
+        return f'Running "job1": {type(self).__name__}'
---
+++ jobs/job2/operator1.py
@@ -0,0 +1,3 @@
+class Operator1(object):
+    def run(self) -> str:
+        return f'Running "job2": {type(self).__name__}'
